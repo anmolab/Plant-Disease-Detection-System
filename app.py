98366204325e10ef3dc7ad9ec0def2bbacf1e8fe
anmolab <99890311+anmolab@users.noreply.github.com>
2024-05-07 00:12:53 +0530

    updated

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def model_prediction(test_image):
 st.header("Disease Recognition")
 test_image = st.file_uploader("Choose an Image:")
 if(st.button("Show Image")):
-    st.image(test_image,width=2,use_column_width=True)
+    st.image(test_image,width=2,use_column_width=False)
 #Predict button
 if(st.button("Predict")):
     st.write("Our Prediction")
